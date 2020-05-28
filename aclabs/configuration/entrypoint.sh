16f61671f19c5b5b7957d3961b8c4a4f39dc3e7a
Maria Sumedre <maria.sumedre@3pillarglobal.com>
2020-05-28 11:08:36 +0300

    lab8.5: migrate and collectstatic on container startup

--- aclabs/configuration/entrypoint.sh
+++ aclabs/configuration/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -ex
 service nginx start
+python manage.py migrate --noinput --settings=aclabs.settings
+python manage.py collectstatic --noinput --settings=aclabs.settings
+
 gunicorn aclabs.wsgi:application --config configuration/gunicorn.conf.py
